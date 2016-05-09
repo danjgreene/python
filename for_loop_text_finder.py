ee482f0708bb354bd2ce68ec71491ec1ce604387
danjgreene <dan.j.greene@gmail.com>
2016-05-09 11:31:34 -0500

    Add text finder

---
+++ for_loop_text_finder.py
@@ -0,0 +1,33 @@
+# Assume text is a variable that
+# holds a string. Write Python code
+# that prints out the position
+# of the first occurrence of 'hoo'
+# in the value of text, or -1 if
+# it does not occur at all.
+
+text = "first hoo" 
+
+# print text.find("boo")
+
+
+def finder():
+    consecutive = 0
+    index = 0
+    final_index = 0
+    for letter in text:
+        # print(consecutive, letter)
+        if letter == 'h':
+            consecutive += 1
+            final_index = index
+        elif letter == 'o' and consecutive != 0:
+            consecutive += 1
+        else:
+            consecutive = 0
+        index += 1
+    if consecutive == 3:
+        return final_index
+    else:
+        return -1
+    
+        
+print finder()
